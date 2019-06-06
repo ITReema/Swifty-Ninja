75b047882bc6b94df525bfee5e693867161a1e01
Reema <it.reema7@gmail.com>
2019-06-06 22:42:20 +0300

    Add files via upload

---
+++ Swifty Ninja/Swifty Ninja/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Swifty Ninja
+//
+//  Created by mac_os on 03/10/1440 AH.
+//  Copyright © 1440 mac_os. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+        
+    }
+
+    
+}
